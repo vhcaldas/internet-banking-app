042d33d7df0210c0161284bf0bf1f68728706ffb
ema_maia@hotmail.com <emamaia@hotmail.com>
2020-04-25 13:19:52 -0300

    adiciona footer e logo ao login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [{
     component: TranferenciaComponent
   }, {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   }, {
     path: '**',
