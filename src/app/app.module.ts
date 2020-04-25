43e3fda919b4f26598c1211d174592d7193dd7d6
ema_maia@hotmail.com <emamaia@hotmail.com>
2020-04-25 16:49:50 -0300

    adiciona formulário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RotaNaoEncontradaComponent } from './rota-nao-encontrada/rota-nao-encontrada.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { LoginComponent } from './login/login.component';
-import { ExtratoComponent } from './extrato/extrato.component';
 import { TranferenciaComponent } from './tranferencia/tranferencia.component';
-import { HomeComponent } from './home/home.component';
-import { RotaNaoEncontradaComponent } from './rota-nao-encontrada/rota-nao-encontrada.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { RotaNaoEncontradaComponent } from './rota-nao-encontrada/rota-nao-encon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  agencia: number;
+  conta: number;
+  password: any;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form){
+    if (!form.valid){
+      form.controls.agencia.markAsTouched();
+      form.controls.password.markAsTouched();
+    }
+    console.log(this.agencia);
+    console.log(this.conta);
+    console.log(this.password);
   }
 
 }
