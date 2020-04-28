7f14c01b4b8489eed84a49dbc69e701afe6f0a63
ema_maia@hotmail.com <emamaia@hotmail.com>
2020-04-28 18:52:36 -0300

    Insere formulário de transferência

--- src/app/transferencia/transferencia.component.ts
+++ src/app/transferencia/transferencia.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+import { ExtratoSaldo } from '../extrato/interface.extratoSaldo';
+import { LoginResponse } from '../login/interface.login.response';
 
 @Component({
   selector: 'app-transferencia',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransferenciaComponent implements OnInit {
 
-  constructor() { }
+  erro = false;
+  cliente: LoginResponse;
+  extratoSaldo: ExtratoSaldo;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.cliente = JSON.parse(localStorage.getItem('cliente'));
+    this.http.get(`https://apiextrato20200428095025.azurewebsites.net/api/extrato/todos/${this.cliente.ContaCorrente}`)
+    .subscribe((extrato: ExtratoSaldo) => {
+      this.extratoSaldo = extrato;
+    });
+}
+
+
+  onSubmit(form){
+    if (!form.valid){
+      form.controls.agencia.markAsTouched();
+      form.controls.contacorrente.markAsTouched();
+      form.controls.senha.markAsTouched();
+    }
+    console.log('Aqui envia');
+  }
+
+  msgError(nomeControle: string, form){
+    if (!form.controls[nomeControle]){
+      return false;
+    }
+    return form.controls[nomeControle].invalid && form.controls[nomeControle].touched;
   }
 
 }
